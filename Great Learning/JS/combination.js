784d930ab3f826467e07af46d7d09459b059d773
sayantanbasu <sayantanbasu@live.in>
2022-06-03 08:56:43 +0530

    last unans

--- Great Learning/JS/combination.js
+++ Great Learning/JS/combination.js
@@ -1,8 +1,14 @@
 var array = ["apple", "banana", "lemon", "mango"];
 
 var result = array.reduce(
-  (acc, v, i) => acc.concat(array.slice(i + 1).map((w) => v + " " + w)),
+  (acc, v, i) => acc.concat(array.slice(i + 1).map((w) => console.log(w))),
   []
 );
 
-console.log(result);
+// const result = array.reduce((acc,item,index) => {
+//     console.log(acc.concat(array.slice(index+1).map(w => item + " " + w )))
+//     // return acc.concat(array.slice(index+1))
+// },[])
+
+
+// console.log(result);
